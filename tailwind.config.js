4adb2c5916fbe215119f9c5723e7516c6fc4fe41
thomas <git@thomasricci.dev>
2023-11-27 14:28:45 -0500

    update config w/ branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#2F2E41",
+        red: "#E06C75",
+        green: "#08C8B6",
+        blue: "#528BFF",
+        purple: "#C678DD",
+        yellow: "#E5C07B"
+      },
+      fontFamily: {
+        sans: ["INTERNAL_SF", ...defaultTheme.fontFamily.sans],
+        serif: ["INTERNAL_NY", ...defaultTheme.fontFamily.serif],
+        mono: ["INTERNAL_JB", ...defaultTheme.fontFamily.mono]
+      },
+    },
   },
   plugins: [],
 }
